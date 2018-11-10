b27253f0e5a80da845695ae13e7a5e76f6a19f1b
Protasov <nikolay.n.protasov@intel.com>
2018-11-10 21:33:38 +0300

    add command buffer class

--- VulkanEngine/VulkanCommandBuffer.cpp
+++ VulkanEngine/VulkanCommandBuffer.cpp
@@ -1,3 +1,5 @@
+#include "VulkanSystem.h"
+
 #include "VulkanCommandBuffer.h"
 
 
@@ -6,7 +8,53 @@ VulkanCommandBuffer::VulkanCommandBuffer()
 {
 }
 
+bool VulkanCommandBuffer::init(VulkanElement* vulkanSystem)
+{
+    //TODO should be count of swapchain Frame Buffer size
+    m_commandBuffers.resize(1); 
+
+    m_commandPool = ((VulkanSystem*)(vulkanSystem))->m_commandPool;
+
+    VkCommandBufferAllocateInfo allocInfo = {};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = m_commandPool->m_commandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = (uint32_t)m_commandBuffers.size();
+
+    if (vkAllocateCommandBuffers(m_commandPool->m_device, &allocInfo, m_commandBuffers.data()) != VK_SUCCESS) {
+        
+        m_init = false;
+
+        return false;
+    }
+
+    return true;
+}
+
+bool VulkanCommandBuffer::destroy()
+{
+    return true;
+}
+
+bool VulkanCommandBuffer::reset(int buffer_number)
+{
+    if (vkResetCommandBuffer(m_commandBuffers[buffer_number], VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT) != VK_SUCCESS)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool VulkanCommandBuffer::free()
+{
+    vkFreeCommandBuffers(m_commandPool->m_device, m_commandPool->m_commandPool, m_commandBuffers.size(), m_commandBuffers.data());
+    return true;
+}
+
+
 
 VulkanCommandBuffer::~VulkanCommandBuffer()
 {
+    destroy();
 }
--- VulkanEngine/VulkanCommandBuffer.h
+++ VulkanEngine/VulkanCommandBuffer.h
@@ -1,8 +1,26 @@
+#ifndef VULKAN_COMMAND_BUFFER
+#define VULKAN_COMMAND_BUFFER
 #pragma once
-class VulkanCommandBuffer
+
+#include <vector>
+
+#include "VulkanElement.h"
+
+class VulkanCommandBuffer: public VulkanElement
 {
 public:
     VulkanCommandBuffer();
+
+    bool init(VulkanElement* vulkanSystem);
+    bool destroy();
+
+    bool reset(int buffer_number);
+    bool free();
+
     ~VulkanCommandBuffer();
+
+    std::vector<VkCommandBuffer> m_commandBuffers;
+    VulkanCommandPool* m_commandPool;
 };
 
+#endif // !VULKAN_COMMAND_BUFFER
